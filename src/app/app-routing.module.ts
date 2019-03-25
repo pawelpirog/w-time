5b3909237e84d811c57c313e5ccb1dff3529c1df
pawelpirog <quexiimetraes@gmail.com>
2019-03-25 22:04:34 +0100

    web development start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WorkerTableComponent} from './worker-table/worker-table.component';
+import {AppComponent} from './app.component';
+import {LogowanieComponent} from './logowanie/logowanie.component';
+import {MasterTableComponent} from './master-table/master-table.component';
+import {AddWorkerComponent} from './add-worker/add-worker.component';
+import {ModifyWorkerComponent} from './modify-worker/modify-worker.component';
+import {FormsModule} from "@angular/forms";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'worker', component: WorkerTableComponent},
+  {path: 'main', component: AppComponent},
+  {path: 'logowanie', component: LogowanieComponent},
+  {path: 'master', component: MasterTableComponent},
+  {path: 'addworker', component: AddWorkerComponent},
+
+  {path: 'modifyworker', component: ModifyWorkerComponent}
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule
+
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+export const routingComponents = [WorkerTableComponent, AppComponent, LogowanieComponent,
+  MasterTableComponent, AddWorkerComponent, ModifyWorkerComponent];
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {WorkerTableComponent} from './worker-table/worker-table.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {LogowanieComponent} from './logowanie/logowanie.component';
+import {MasterTableComponent} from './master-table/master-table.component';
+import {AddWorkerComponent} from './add-worker/add-worker.component';
+import {ModifyWorkerComponent} from './modify-worker/modify-worker.component';
+import {RandomthingComponent} from './randomthing/randomthing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    routingComponents,
+    FooterComponent,
+    HeaderComponent,
+    MasterTableComponent,
+    AddWorkerComponent,
+    ModifyWorkerComponent,
+    RandomthingComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+    LogowanieComponent,
+    WorkerTableComponent,
+    MasterTableComponent,
+    AddWorkerComponent]
 })
 export class AppModule { }
